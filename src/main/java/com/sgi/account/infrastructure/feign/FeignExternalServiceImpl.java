747f4ac791fd4ddf2c43d09c890b128e8d5012c0
uriartex12 <prietomatex_1998@hotmail.com>
2024-12-31 20:11:26 -0500

    config circuitbreak

--- src/main/java/com/sgi/account/infrastructure/feign/FeignExternalServiceImpl.java
+++ src/main/java/com/sgi/account/infrastructure/feign/FeignExternalServiceImpl.java
@@ -3,10 +3,12 @@ import com.sgi.account.domain.ports.out.FeignExternalService;
 import com.sgi.account.domain.shared.CustomError;
 import com.sgi.account.infrastructure.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 
 import static com.sgi.account.domain.shared.Constants.EXTERNAL_REQUEST_ERROR_FORMAT;
 import static com.sgi.account.domain.shared.Constants.EXTERNAL_REQUEST_SUCCESS_FORMAT;
@@ -20,9 +22,11 @@ import static com.sgi.account.domain.shared.Constants.EXTERNAL_REQUEST_SUCCESS_F
 public class FeignExternalServiceImpl implements FeignExternalService {
 
     private final WebClient webClient;
+    private final ReactiveCircuitBreakerFactory circuitBreakerFactory;
 
-    public FeignExternalServiceImpl(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.build();
+    public FeignExternalServiceImpl( ReactiveCircuitBreakerFactory circuitBreakerFactory) {
+        this.webClient = WebClient.builder().build();
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     @Override
@@ -56,17 +60,32 @@ public class FeignExternalServiceImpl implements FeignExternalService {
 
     @Override
     public <R> Mono<R> getMono(String url, String pathVariable, Class<R> responseType) {
-        return webClient.get()
-                .uri(url, pathVariable)
-                .retrieve()
-                .bodyToMono(responseType)
-                .doOnNext(response -> logSuccess(url, response))
-                .doOnError(ex ->
-                        log.error(EXTERNAL_REQUEST_ERROR_FORMAT, url, ex))
-                .onErrorResume(ex -> {
-                        log.error(EXTERNAL_REQUEST_ERROR_FORMAT, url, ex);
-                    return Mono.error(new CustomException(CustomError.E_OPERATION_FAILED));
-                });
+        ReactiveCircuitBreaker circuitBreaker = circuitBreakerFactory.create("recommended");
+        return circuitBreaker.run(
+                webClient.get()
+                        .uri(url, pathVariable)
+                        .retrieve()
+                        .bodyToMono(responseType)
+                        .doOnNext(response -> logSuccess(url, response))
+                        .doOnError(ex -> log.error(EXTERNAL_REQUEST_ERROR_FORMAT, url, ex))
+                        .onErrorResume(ex -> {
+                            log.error(EXTERNAL_REQUEST_ERROR_FORMAT, url, ex);
+                            return Mono.error(new CustomException(CustomError.E_OPERATION_FAILED));
+                        }),
+                throwable -> {
+                    log.error("Error during circuit breaker execution", throwable);
+                    return Mono.just(createDefaultValue(responseType));
+                }
+        );
+    }
+
+    private <R> R createDefaultValue(Class<R> responseType) {
+        try {
+            return responseType.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            log.error("Error creating default value for response type: " + responseType.getName(), e);
+            return null;
+        }
     }
 
     private <R> void logSuccess(String url, R response) {
